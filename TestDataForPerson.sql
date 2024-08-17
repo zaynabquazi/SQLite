4ba6bbbf5dcb203b894a609e65eccdcfe4cb1beb
zaynabquazi <zmquazi@pipeline.sbcc.edu>
2024-08-17 14:55:12 -0700

    person

---
+++ TestDataForPerson.sql
@@ -0,0 +1,14 @@
+-- TestDataForPerson 
+
+INSERT INTO "Person" ("Name", "Key") VALUES ('John Doe', 1);
+INSERT INTO "Person" ("Name", "Key") VALUES ('Jane Smith', 2);
+INSERT INTO "Person" ("Name", "Key") VALUES ('Michael Johnson', 3);
+INSERT INTO "Person" ("Name", "Key") VALUES ('Emily Davis', 4);
+INSERT INTO "Person" ("Name", "Key") VALUES ('David Martinez', 5);
+INSERT INTO "Person" ("Name", "Key") VALUES ('Laura Wilson', 6);
+INSERT INTO "Person" ("Name", "Key") VALUES ('James Brown', 7);
+INSERT INTO "Person" ("Name", "Key") VALUES ('Sophia Taylor', 8);
+INSERT INTO "Person" ("Name", "Key") VALUES ('Daniel Harris', 9);
+INSERT INTO "Person" ("Name", "Key") VALUES ('Olivia Lewis', 10);
+
+
