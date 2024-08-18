ed1b6cb4c1d15b4199250c1cdfc5fe3c7df2f05c
Yaheya Quazi <yaheya@msn.com>
2024-08-17 17:02:02 -0700

    changed readme

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Practicing making Database Structures and executing SQL code.
 
 
 We have Phone Book with various contacts and locations. We practice how to join multiple tables and find duplicates etc. 
+
+*Contributors : Yaheya Quazi
+*Contributor: Zaynab Quazi
--- select-phonenumbers.sql
+++ select-phonenumbers.sql
@@ -1,2 +1,10 @@
 -- SQLite
 select * from phonenumbers;
+select * from phonenumbers where phonenumber like '%555%';
+select * from phonenumbers where phonenumber like '%555%' and phonenumber like '%555%'; 
+
+
+SELECT phonenumber, COUNT(*)
+FROM phonenumbers
+WHERE phonenumber LIKE '%555%'
+GROUP BY phonenumber;
