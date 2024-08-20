808bc5fb835f2777236336ec4fe839fa42fb1b6a
zaynabquazi <zmquazi@pipeline.sbcc.edu>
2024-08-20 16:40:12 -0700

    hw

---
+++ HW.sql
@@ -0,0 +1,13 @@
+-- print customer details 
+
+-- print what customer has purchased 
+
+-- print all orders (product name, customer name, quantity, order date) 
+
+-- print which item sold the most and product name 
+
+-- insert some records in all tables 
+
+-- update random data in all tables 
+
+
