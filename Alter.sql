1975c50cd68fe9b55f158f7dcd2cf2efa132cbd9
zaynabquazi <zmquazi@pipeline.sbcc.edu>
2024-08-20 15:33:13 -0700

    alter tables

--- Alter.sql
+++ Alter.sql
@@ -3,3 +3,19 @@ ADD COLUMN inventory_id INTEGER;
 
 ALTER TABLE orders
 ADD FOREIGN KEY (inventory_id) REFERENCES inventory(id);
+
+--alter customers table 
+
+ALTER TABLE customer 
+RENAME COLUMN ID TO customer_id;
+
+--alter inventory table 
+
+ALTER TABLE inventory 
+RENAME COLUMN id to inventory_id;
+
+--alter orders table 
+
+ALTER TABLE orders 
+RENAME COLUMN id to order_id 
+
