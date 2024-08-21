70a93e5982b538f83294c6f3b4ecb2898e04588b
Yaheya Quazi <yaheya@msn.com>
2024-08-21 14:24:28 -0700

    Added place holder to add Phone number into create customer form

--- flask_crud_app/app.py
+++ flask_crud_app/app.py
@@ -10,7 +10,7 @@ class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), nullable=False)
-
+  
 with app.app_context():
     db.create_all()
 
