5af0dd4111e2bbe0ce392453adf070ab019fc956
Yaheya Quazi <yaheya@msn.com>
2024-08-22 10:06:13 -0700

    updates to python code

--- flask_crud_app/setup_db.py
+++ flask_crud_app/setup_db.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import inspect
 
 # Initialize the Flask application
 app = Flask(__name__)
@@ -17,3 +18,20 @@ class Customer(db.Model):
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), nullable=False)
     phone = db.Column(db.String(100), nullable=True)
+
+# Define the Inventory model, representing the 'inventory' table in the database
+class Inventory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.String(100), nullable=False)
+    quantity = db.Column(db.String(100), nullable=True)
+
+# Create the tables if they don't exist
+ 
+    with app.app_context():
+        inspector = inspect(db.engine)
+        if 'inventory' not in inspector.get_table_names():
+            db.create_all()
+        if 'customer' not in inspector.get_table_names():
+            db.create_all()
+
