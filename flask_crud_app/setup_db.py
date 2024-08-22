130b6ae78c66aa41fafae2bf7dfd84f66ea06859
Yaheya Quazi <yaheya@msn.com>
2024-08-22 10:09:14 -0700

    db.createall already checks if the tables exists or not

--- flask_crud_app/setup_db.py
+++ flask_crud_app/setup_db.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import inspect
 
 # Initialize the Flask application
 app = Flask(__name__)
@@ -28,10 +27,7 @@ class Inventory(db.Model):
 
 # Create the tables if they don't exist
  
-    with app.app_context():
-        inspector = inspect(db.engine)
-        if 'inventory' not in inspector.get_table_names():
-            db.create_all()
-        if 'customer' not in inspector.get_table_names():
-            db.create_all()
+    # Create the tables if they don't exist
+with app.app_context():
+    db.create_all()
 
