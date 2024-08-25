0c5c819743149803d52c52f49fbb520cc2c5d886
Yaheya Quazi <yaheya@msn.com>
2024-08-25 13:59:53 -0700

    code updates

--- Blog/app.py
+++ Blog/app.py
@@ -1,18 +1,16 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+import routes
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///gluon.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'your_secret_key'
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
-# Import models and routes
-from models import Post, User
-import routes
 
 @login_manager.user_loader
 def load_user(user_id):
